cae4e95593df863ad226a4b7bf45310a2dba2165
Juan Leonel Mitre <juanleonel.developer@gmail.com>
2024-07-10 23:01:06 -0500

    deleted:    auth.php 	modified:   carrito.php 	modified:   global/config.php 	modified:   index.php 	modified:   mostrarCarrito.php 	modified:   pagar.php 	renamed:    tienda.sql -> storebspty.sql 	modified:   templates/cabecera.php 	modified:   verificador.php

--- global/config.php
+++ global/config.php
@@ -3,7 +3,7 @@
 define("SERVIDOR", "localhost");//est
 define("USUARIO", "root");//aqui usted pone su usuario de la base de datos MYSQL phpMyAdmin
 define("PASSWORD", ""); //aqui usted pone su contraseña de la base de datos MYSQL phpMyAdmin
-define("BD", "tienda");
+define("BD", "storebspty");
 
 define("KEY", "develoteca");
 define("COD", "AES-128-ECB");
--- index.php
+++ index.php
@@ -4,27 +4,19 @@ include('global/conexion.php');
 include('carrito.php');
 include("templates/cabecera.php");
 ?>
-
-<?php echo $mensaje ?>
 <div class="row justify-content-sm-center overflow-hidden">
-    <?php
-    $sentencia = $pdo->prepare("SELECT * FROM productos");
-    $sentencia->execute();
-    $listaProd = $sentencia->fetchAll(PDO::FETCH_ASSOC);
 
-    foreach ($listaProd as $producto) :
-    ?>
         <div class="col-4">
             <div class="card p-2 m-2 ">
-                <img class="card-img-top" src="<?php echo $producto['imagen'] ?>" alt="Title" />
+                <img class="card-img-top" src="" alt="Title" />
                 <div class="card-body">
-                    <h3 class="card-title"><?php echo $producto['nombre'] ?></h3>
-                    <span class="card-title"><?php echo $producto['precio'] ?>USD</span>
+                    <h3 class="card-title">Articulo</h3>
+                    <span class="card-title">300USD</span>
                     <form action="" method="post">
-                        <input type="hidden" name="nombre" id="nombre" value="<?php echo openssl_encrypt($producto['nombre'], COD, KEY) ?>">
-                        <input type="hidden" name="precio" id="precio" value="<?php echo openssl_encrypt($producto['precio'], COD, KEY) ?>">
-                        <input type="hidden" name="id" id="id" value="<?php echo openssl_encrypt($producto['id'], COD, KEY) ?>">
-                        <input type="hidden" name="cantidad" id="cantidad" value="<?php echo openssl_encrypt(1, COD, KEY); ?>">
+                        <input type="hidden" name="nombre" id="nombre" value="">
+                        <input type="hidden" name="precio" id="precio" value="">
+                        <input type="hidden" name="id" id="id" value="">
+                        <input type="hidden" name="cantidad" id="cantidad" value="">
 
                         <button class="btn btn-primary btn-lg" type="submit" value="agregar" name="btnAccion">
                             <i class="fa-solid fa-cart-shopping"></i>
@@ -33,7 +25,7 @@ include("templates/cabecera.php");
                 </div>
             </div>
         </div>
-    <?php endforeach ?>
+
 </div>
 
 <?php
